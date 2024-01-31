d2478c6635cde9fec4305848c19dde473364537e
Shane Beaudoin <157167429+BitterSweetsss@users.noreply.github.com>
2024-01-31 09:07:25 -0500

    1/31/2024

--- src/main/java/Game/NPC.java
+++ src/main/java/Game/NPC.java
@@ -1,4 +1,17 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NPC {
+    public static void main(String [] args)
+    ArrayList<string> NPC = new ArrayList<string>();//adding the multiple NPCs to the list
+    {
+        NPC.add("skeleton");
+        NPC.add("slime");
+        NPC.add("zombie");
+        NPC.add("undead mage");
+        NPC.add("shopkeeper");
+    }
 }
