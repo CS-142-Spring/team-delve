03a7383a93034dfee9264fe98835e9eece363454
Shane Beaudoin <157167429+BitterSweetsss@users.noreply.github.com>
2024-02-03 15:02:13 -0500

    added attack rolling

--- src/main/java/Game/NPC.java
+++ src/main/java/Game/NPC.java
@@ -1,18 +1,24 @@
 package Game;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
 public class NPC {
-    public static void main(String [] args) {
+    public static int main(String [] args) {
         ArrayList<String> NPC = new ArrayList<>();//adding the multiple NPCs to the list
         {
             NPC.add("skeleton");
             NPC.add("slime");
             NPC.add("zombie");
             NPC.add("undead mage");
-            NPC.add("shopkeeper");
+        }
+        int min=3; //min and max number in order for damage roll
+        int max=8;
+
+        int attack(int min, int max) {
+            Random random = new Random();
+            return random.ints(min, max)
+                    .findFirst()
+                    .getAsInt();
         }
     }
 }
