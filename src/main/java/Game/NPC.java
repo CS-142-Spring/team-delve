ed39e67b13b671545b0252e4fd56375e776fef12
Shane Beaudoin <157167429+BitterSweetsss@users.noreply.github.com>
2024-02-03 15:36:26 -0500

    added attack rolling

--- src/main/java/Game/NPC.java
+++ src/main/java/Game/NPC.java
@@ -1,9 +1,10 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class NPC {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         ArrayList<String> NPC = new ArrayList<>();//adding the multiple NPCs to the list
         {
             NPC.add("skeleton");
@@ -11,5 +12,15 @@ public class NPC {
             NPC.add("zombie");
             NPC.add("undead mage");
         }
+        Random attack_dice = new Random();// simple dice roll for attack
+        int attack;
+        for (int counter = 1; counter <= 6; counter++) {
+            attack = 1 + attack_dice.nextInt(6);
         }
+        Random health_dice = new Random();//rolling for health
+        int health;
+        for (int counter = 5; counter <= 10; counter++) {
+            health = 10 + health_dice.nextInt(6);
+        }
+    }
 }
