04e0a10d5889099fbaa4ff256ecc7422575342f3
Shane Beaudoin <157167429+BitterSweetsss@users.noreply.github.com>
2024-02-14 01:29:23 -0500

    added multiple npc values

--- src/main/java/Game/NPC.java
+++ src/main/java/Game/NPC.java
@@ -14,13 +14,34 @@ public class NPC {
         }
         Random attack_dice = new Random();// simple dice roll for attack
         int attack;
-        for (int counter = 1; counter <= 6; counter++) {
-            attack = 1 + attack_dice.nextInt(6);
+        int NPC_Strength; // this is for the monsters added strength of the attack
+        int NPC_Health;
+        if (NPC  = "skeleton"){
+            System.out.println("there is a skeleton in the room");
+            NPC_Strength= 3;
+            NPC_Health = 20;
         }
-        Random health_dice = new Random();//rolling for health
-        int health;
-        for (int counter = 5; counter <= 10; counter++) {
-            health = 10 + health_dice.nextInt(6);
+        else if (NPC = "slime") {
+            System.out.println("there is a slime in the room");
+            NPC_Strength = 1;
+            NPC_Health = 10;
+        }
+        else if (NPC = "zombie"){
+            System.out.println("there is a zombie in the room");
+            NPC_Strength= 2;
+            NPC_Health = 30;
+        }
+        else  if (NPC = "undead mage") {
+            System.out.println("there is a zombie in the room");
+            NPC_Strength = 7;
+            NPC_Health = 40;
+        }
+        else{
+            System.out.println("there is nothing alive in the room")
+        }
+
+        for (int counter = 1; counter <= 6; counter++) {
+            attack = 1 + strength + attack_dice.nextInt(6);
         }
     }
 }
